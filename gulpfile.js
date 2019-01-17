84bcbb1d1d5f1c78c46f4249fddc1abb494c0222
zsv <zsv@linuxwizard.ru>
2019-01-17 15:07:59 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin');
-const pngquant = require('imagemin-pngquant');
 const cssnano = require("gulp-cssnano");
 const plumber = require("gulp-plumber");
 
 gulp.task("scss", () => {
   return gulp
-    .src("dev/scss/**/*.scss")
+    .src("src/stylesheets/**/*.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(
@@ -19,27 +16,9 @@ gulp.task("scss", () => {
       })
     )
     .pipe(cssnano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest("public/css"));
+    .pipe(gulp.dest("public/stylesheets"));
 });
 
-
-gulp.task('img', function() {
-  return gulp
-    .src('dev/images/**/*')
-    .pipe(
-    imagemin({
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngquant()],
-      interlaced: true,
-    })
-    )
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('public/images'));
-  });
-
-gulp.task("default", ["scss", "img"], () => {
-  gulp.watch("dev/scss/**/*.scss", ["scss"]);
-  gulp.watch("dev/images/**/*", ["img"]);
+gulp.task("default", ["scss"], () => {
+  gulp.watch("src/stylesheets/**/*.scss", ["scss"]);
 });
